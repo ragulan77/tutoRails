35ada53322219e018555fbab7a322c80b13a436b
ragulan charles <ragulan.charlesm@gmail.com>
2013-04-29 10:33:26 +0200

    End chap9

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
                         password: "foobar", password_confirmation: "foobar") 
   end
   subject { @user }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -26,6 +28,16 @@ describe User do
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
+  it { should_not be_admin }
+  
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
   
   describe "when name is not present" do
       before { @user.name = " " }
